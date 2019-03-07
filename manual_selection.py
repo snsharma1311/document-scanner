9d505be3951d91245d7aa42113b3c6ba8d86569a
Shashank Sharma <snsharma1311@gmail.com>
2019-03-07 11:47:14 +0530

    Update manual_selection.py

--- manual_selection.py
+++ manual_selection.py
@@ -2,6 +2,7 @@
 Filename: manual_selection.py
 Usage: Select document boundaries manually 
 Author: Shashank Sharma
+Reference: https://www.pyimagesearch.com/2014/09/01/build-kick-ass-mobile-document-scanner-just-5-minutes/
 """
 import imutils
 import cv2
